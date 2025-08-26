4d41628260252f8ad01db69b8d1caed28e2abdc0
Jonas <jhoucdn@gmail.com>
2025-08-25 20:03:56 -0400

    Atualizando o código para obter mais de dois valores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,34 @@
-const form = document.querySelector('#valores');
+const form = document.querySelector('#form');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     
-    var opcao1 = document.getElementById('opcao1').value;
-    var opcao2 = document.getElementById('opcao2').value;
-    
-    if (!opcao1) {
-        setResult('Digite uma primeira opção', false);
-        return;
-    }
-    if (!opcao2) {
-        setResult('Digite uma segunda opção', false);
-        return;
-    }
+   const input = document.getElementById('option').value;
 
-    let escolha;
-    escolha = getRandomValue();
+    const opcoes = input.split('\n').map(op => op.trim()).filter(op => op !== '');
 
-    console.log(escolha);
+    const indice = getRandomValue(opcoes.length);
+    const escolha = opcoes[indice];
 
-    switch (escolha) {
-        case 0:
-            escolha = opcao1;
-            break;
-        case 1:
-            escolha = opcao2;
-            break;
-        default:
-            break;
-    }
-    const msg = `${escolha}`;
-    setResult(msg, true);
+    setResult(`Escolhido: ${escolha}`, true);
 });
 
-function getRandomValue() {
-    let numero = Math.floor(Math.random() * 2);
+function getRandomValue(max) {
+    let numero = Math.floor(Math.random() * max);
     return numero;
 }
 
-function createp() {
+function createPResult() {
     const p = document.createElement('p');
     return p;
 }
 
 function setResult(msg, isValid) {
     const result = document.querySelector('#result');
+    result.style.background = "white"
     result.innerHTML = '';
-    const p = createp();
-    if (isValid) {
-        p.classList.add('resultado');
-    } else {
-        p.classList.add('error');
-    }
+    const p = createPResult();
+    p.classList.add(isValid ? 'resultado' : 'error');
     p.innerHTML = msg;
     result.appendChild(p);
-
-
 }
-
-
-
-
